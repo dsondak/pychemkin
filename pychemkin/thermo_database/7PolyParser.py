c97489069329cee7097cc2b884c8bd5ebb59ca77
Riddhi Shah <riddhishah@riddhis-mbp.lan>
2018-03-01 20:08:45 -0500

    Add parser for dict to xml

--- pychemkin/thermo_database/7PolyParser.py
+++ pychemkin/thermo_database/7PolyParser.py
@@ -1,6 +1,10 @@
 import re
 from copy import deepcopy
 import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import Element, SubElement, Comment
+from xml.etree.ElementTree import XMLID
+from xml.etree import ElementTree
+from xml.dom import minidom
 
 class Parser_7_coeffs:
     """
@@ -10,6 +14,10 @@ class Parser_7_coeffs:
     import re
     from copy import deepcopy
     import xml.etree.ElementTree as ET
+    from xml.etree.ElementTree import Element, SubElement, Comment
+    from xml.etree.ElementTree import XMLID
+    from xml.etree import ElementTree
+    from xml.dom import minidom
 
     The default txt file to parse the 7 polynomialreactions from is defaulted in initialization:
     7poly_scrapper_output.txt which is created by Scrapers.py
@@ -23,9 +31,9 @@ class Parser_7_coeffs:
     A database table with the columns: molecular wight, specie name, state,
     low tmin, low thigh,high tmin, high thigh, low coeffs, high coeffs
     """
-    
+
     def __init__(self,f='7poly_scrapper_output.txt'):
-        self.txt_file = file
+        self.txt_file = f
 
     def species_txt_to_dict(self):
         #read the file
@@ -93,3 +101,50 @@ class Parser_7_coeffs:
                 species_info[specie,specie_state]['high']['Tmin'] = high_min
                 species_info[specie,specie_state]['high']['coeffs'] = high_coeffs
         return species_info
+
+    def prettify(self, elem):
+        """Return a pretty-printed XML string for the Element.
+        """
+        rough_string = ElementTree.tostring(elem, 'utf-8')
+        reparsed = minidom.parseString(rough_string)
+        return reparsed.toprettyxml(indent="  ")
+
+    def species_dict_to_xml(self):
+
+        species_info = species_txt_to_dict()
+
+        #create root for xml file
+        root = Element('specieData')
+        root.set('id','specie_data')
+        comment = Comment('Created by Riddhi Shah')
+        root.append(comment)
+
+        for i,k in enumerate(species_info):
+            #create a specie entry in xml
+            specie = Element('specie')
+            specie.set('name',str(k[0]))
+            specie.set('state',str(k[1]))
+            specie.set('Mweight','N/A')
+
+            thermo = SubElement(specie, 'thermo')
+
+            NASA_low = SubElement(thermo, 'NASA')
+            NASA_low.set('Tmin',str(species_info[k]['low']['Tmin']))
+            NASA_low.set('Tmax',str(species_info[k]['low']['Tmax']))
+            floatArray = SubElement(NASA_low, 'floatArray')
+            floatArray.set('name','coeffs')
+            low_coeffs =", ".join(species_info[k]['low']['coeffs'])
+            floatArray.text = low_coeffs
+
+            NASA_HIGH = SubElement(thermo, 'NASA')
+            NASA_HIGH.set('Tmin',str(species_info[k]['high']['Tmin']))
+            NASA_HIGH.set('Tmax',str(species_info[k]['high']['Tmax']))
+            floatArray = SubElement(NASA_HIGH, 'floatArray')
+            floatArray.set('name','coeffs')
+            high_coeffs =", ".join(species_info[k]['high']['coeffs'])
+            floatArray.text = high_coeffs
+
+            #add specie to root
+            root.append(specie)
+            
+        return prettify(root))
