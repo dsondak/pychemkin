1e015ff6b7474556c1725f186066c711257981ac
hannahsim <ssim@g.harvard.edu>
2018-03-29 15:02:18 -0400

    Cleaned up tests for reaction systems.

--- tests/test_ReactionSystems.py
+++ tests/test_ReactionSystems.py
@@ -1,10 +1,11 @@
 
-"""Test module for """
+"""Test module for reaction systems"""
 
 import numpy
 import os
 import pytest
 import warnings
+warnings.simplefilter("error")
 
 from pychemkin.config import DB_DIRECTORY
 from pychemkin.parsers.XMLParser import XMLParser
@@ -14,13 +15,9 @@ from pychemkin.reactions.ReactionSystems import ReactionSystem
 TEST_DB_PATH = os.path.join(DB_DIRECTORY + "/NASA7_coeffs.sqlite")
 
 
-# Treat warnings like errors (for testing purposes)
-warnings.simplefilter("error")
-
-
 @pytest.fixture
-def test_rxn_sys():
-    """Returns a valid reaction system"""
+def test_lowT_rxn_sys():
+    """Returns a valid reaction system at low T"""
     xml_filename =  "tests/test_xml_files/rxn.xml"
     xml_parser = XMLParser(xml_filename)
     species = xml_parser.get_species()
@@ -33,6 +30,87 @@ def test_rxn_sys():
     rxnsys = ReactionSystem(xml_parser.reaction_list, thermo_coeffs, temp, concentrations)
     return rxnsys
 
+@pytest.fixture
+def test_highT_rxn_sys():
+    """Returns a valid reaction system at high T"""
+    xml_filename =  "tests/test_xml_files/rxn.xml"
+    xml_parser = XMLParser(xml_filename)
+    species = xml_parser.get_species()
+
+    sql_parser = SQLParser(TEST_DB_PATH, species)
+    thermo_coeffs = sql_parser.get_thermo_coeffs()
+
+    temp = 5000 # "high" temperature range in NASA coeffs database
+    concentrations = {'H':1, 'O2':1, 'H2O':1}
+    rxnsys = ReactionSystem(xml_parser.reaction_list, thermo_coeffs, temp, concentrations)
+    return rxnsys
+
+@pytest.fixture
+def test_rev_rxn_sys():
+    """Returns a valid reaction system with reversible reaction(s)."""
+    xml_filename =  "tests/test_xml_files/rev_rxn.xml"
+    xml_parser = XMLParser(xml_filename)
+    species = xml_parser.get_species()
+    sql_parser = SQLParser(TEST_DB_PATH, species)
+    thermo_coeffs = sql_parser.get_thermo_coeffs()
+    temp = 500 # "low" temperature range in NASA coeffs database
+    concentrations = {'H':1, 'O2':1, 'H2O':1}
+    rxnsys = ReactionSystem(xml_parser.reaction_list, thermo_coeffs, temp, concentrations)
+    return rxnsys
+
+def test_rxn_system_functionalities(test_lowT_rxn_sys):
+    """Test functions in reaction system at low T."""
+    
+    # Test sort_reaction_rates() routine
+    rates = test_lowT_rxn_sys.sort_reaction_rates()
+    assert rates['H'] == -30.
+    assert rates['O2'] == -15.
+    assert rates['H2O'] == 30.
+
+    # Test fetching of low temperature NASA matrix
+    expected_lowT_nasa = {'H': numpy.array([2.50000000e+00, 0.00000000e+00,
+                                      0.00000000e+00, 0.00000000e+00,
+                                      0.00000000e+00, 2.54716270e+04,
+                                      -4.60117608e-01]),
+                    'H2O': numpy.array([ 3.38684249e+00, 3.47498246e-03,
+                                       -6.35469633e-06, 6.96858127e-09,
+                                       -2.50658847e-12, -3.02081133e+04,
+                                       2.59023285e+00]),
+                    'O2': numpy.array([3.21293640e+00, 1.12748635e-03,
+                                      -5.75615047e-07, 1.31387723e-09,
+                                      -8.76855392e-13, -1.00524902e+03,
+                                      6.03473759e+00])}
+    assert (numpy.isclose(test_lowT_rxn_sys.NASA_matrix['H2O'],
+                          expected_lowT_nasa['H2O'], atol=1e-16)).all()
+    assert (numpy.isclose(test_lowT_rxn_sys.NASA_matrix['O2'],
+                          expected_lowT_nasa['O2'], atol=1e-16)).all()
+    assert (numpy.isclose(test_lowT_rxn_sys.NASA_matrix['H'],
+                          expected_lowT_nasa['H'], atol=1e-16)).all()
+
+
+def test_highT_rxn_system_functionaltiies(test_highT_rxn_sys):
+    """Test functions in reaction system at high T."""
+
+    # Test fetching of high temperature NASA matrix
+    expected_highT_nasa = ({'O2': numpy.array([3.69757819e+00, 6.13519689e-04,
+                                              -1.25884199e-07, 1.77528148e-11,
+                                              -1.13643531e-15, -1.23393018e+03,
+                                              3.18916559e+00]),
+                           'H2O': numpy.array([2.67214561e+00, 3.05629289e-03,
+                                              -8.73026011e-07, 1.20099639e-10,
+                                              -6.39161787e-15, -2.98992090e+04,
+                                              6.86281681e+00]),
+                           'H': numpy.array([2.50000000e+00, 0.00000000e+00,
+                                            0.00000000e+00, 0.00000000e+00,
+                                            0.00000000e+00, 2.54716270e+04,
+                                            -4.60117638e-01])})
+    assert (numpy.isclose(test_highT_rxn_sys.NASA_matrix['H2O'],
+                          expected_highT_nasa['H2O'], atol=1e-16)).all()
+    assert (numpy.isclose(test_highT_rxn_sys.NASA_matrix['O2'],
+                          expected_highT_nasa['O2'], atol=1e-16)).all()
+    assert (numpy.isclose(test_highT_rxn_sys.NASA_matrix['H'], 
+                          expected_highT_nasa['H'], atol=1e-16)).all()
+
 def test_rxn_sys_invalid_temperature():
     """Tests setting up reaction system with invalid temperatures."""
     xml_filename =  "tests/test_xml_files/rxns_mixed.xml"
@@ -41,21 +119,14 @@ def test_rxn_sys_invalid_temperature():
     sql_parser = SQLParser(TEST_DB_PATH, species)
     thermo_coeffs = sql_parser.get_thermo_coeffs()
     concentrations = {'H':1, 'O2':2, 'OH':1, 'O':4, 'H2O':0, 'H2':1}
+    
     temp = 0
     with pytest.raises(ValueError):
         rxnsys = ReactionSystem(xml_parser.reaction_list, thermo_coeffs, temp, concentrations)
+    
     temp = -100
     with pytest.raises(ValueError):
         rxnsys = ReactionSystem(xml_parser.reaction_list, thermo_coeffs, temp, concentrations)
-
-
-def test_rxn_sys_get_reaction_rate_for_1_rxn(test_rxn_sys):
-    """Tests function to get reaction rate for a given system of reactions (just 1 reaction)."""
-    rates = test_rxn_sys.sort_reaction_rates()
-    assert rates['H'] == -30.
-    assert rates['O2'] == -15.
-    assert rates['H2O'] == 30.
-
 
 def test_rxn_sys_get_reaction_rate_for_3_rxns():
     """Tests function to get reaction rate for a given system of reactions (more than 1 reaction)."""
@@ -75,65 +146,8 @@ def test_rxn_sys_get_reaction_rate_for_3_rxns():
     assert rates['O'] == -10.
     assert rates['OH'] == 0.
 
-
-def test_rxn_sys_get_lowT_nasa_matrix(test_rxn_sys):
-    """Tests function to fetch NASA coefficients of appropriate T and appropriate species in reaction."""
-    # Order of low T range NASA coefficients: H, H2O, O2
-    expected_nasa = {'H': numpy.array([2.50000000e+00, 0.00000000e+00,
-                                      0.00000000e+00, 0.00000000e+00,
-                                      0.00000000e+00, 2.54716270e+04,
-                                      -4.60117608e-01]),
-                    'H2O': numpy.array([ 3.38684249e+00, 3.47498246e-03,
-                                       -6.35469633e-06, 6.96858127e-09,
-                                       -2.50658847e-12, -3.02081133e+04,
-                                       2.59023285e+00]),
-                    'O2': numpy.array([3.21293640e+00, 1.12748635e-03,
-                                      -5.75615047e-07, 1.31387723e-09,
-                                      -8.76855392e-13, -1.00524902e+03,
-                                      6.03473759e+00])}
-    assert (numpy.isclose(test_rxn_sys.NASA_matrix['H2O'], expected_nasa['H2O'])).all()
-    assert (numpy.isclose(test_rxn_sys.NASA_matrix['O2'], expected_nasa['O2'])).all()
-    assert (numpy.isclose(test_rxn_sys.NASA_matrix['H'], expected_nasa['H'])).all()
-
-
-def test_rxn_sys_get_highT_nasa_matrix():
-    """Tests function to fetch NASA coefficients of appropriate T and appropriate species in reaction."""
-    xml_filename =  "tests/test_xml_files/rxn.xml"
-    xml_parser = XMLParser(xml_filename)
-    species = xml_parser.get_species()
-    sql_parser = SQLParser(TEST_DB_PATH, species)
-    thermo_coeffs = sql_parser.get_thermo_coeffs()
-    temp = 5000 # "high" temperature range in NASA coeffs database
-    concentrations = {'H':1, 'O2':1, 'H2O':1}
-    rxnsys = ReactionSystem(xml_parser.reaction_list, thermo_coeffs, temp, concentrations)
-    # Order of high T range NASA coefficients: H, H2O, O2
-    expected_nasa = {'O2': numpy.array([3.69757819e+00, 6.13519689e-04,
-                                       -1.25884199e-07, 1.77528148e-11,
-                                       -1.13643531e-15, -1.23393018e+03,
-                                       3.18916559e+00]),
-                    'H2O': numpy.array([2.67214561e+00, 3.05629289e-03,
-                                       -8.73026011e-07, 1.20099639e-10,
-                                       -6.39161787e-15, -2.98992090e+04,
-                                       6.86281681e+00]),
-                    'H': numpy.array([2.50000000e+00, 0.00000000e+00,
-                                     0.00000000e+00, 0.00000000e+00,
-                                     0.00000000e+00, 2.54716270e+04,
-                                     -4.60117638e-01])}
-    assert (numpy.isclose(rxnsys.NASA_matrix['H2O'], expected_nasa['H2O'])).all()
-    assert (numpy.isclose(rxnsys.NASA_matrix['O2'], expected_nasa['O2'])).all()
-    assert (numpy.isclose(rxnsys.NASA_matrix['H'], expected_nasa['H'])).all()
-
-
-def test_rxn_sys_rev_reaction():
+def test_rxn_sys_rev_reaction(test_rev_rxn_sys):
     """Tests setting up reaction system with reversible reaction."""
-    xml_filename =  "tests/test_xml_files/rev_rxn.xml"
-    xml_parser = XMLParser(xml_filename)
-    species = xml_parser.get_species()
-    sql_parser = SQLParser(TEST_DB_PATH, species)
-    thermo_coeffs = sql_parser.get_thermo_coeffs()
-    temp = 500 # "low" temperature range in NASA coeffs database
-    concentrations = {'H':1, 'O2':1, 'H2O':1}
-    rxnsys = ReactionSystem(xml_parser.reaction_list, thermo_coeffs, temp, concentrations)
     expected_nasa = {'O2': numpy.array([3.21293640e+00, 1.12748635e-03,
                                        -5.75615047e-07, 1.31387723e-09,
                                        -8.76855392e-13, -1.00524902e+03,
@@ -146,13 +160,12 @@ def test_rxn_sys_rev_reaction():
                                      0.00000000e+00, 0.00000000e+00,
                                      0.00000000e+00, 2.54716270e+04,
                                      -4.60117608e-01])}
-    rev_rxn_obj = xml_parser.reaction_list[0]
+    rev_rxn_obj = test_rev_rxn_sys.reaction_list[0]
     assert (numpy.isclose(rev_rxn_obj.NASA_poly_coefs_dict['H2O'], expected_nasa['H2O'])).all()
     assert (numpy.isclose(rev_rxn_obj.NASA_poly_coefs_dict['O2'], expected_nasa['O2'])).all()
     assert (numpy.isclose(rev_rxn_obj.NASA_poly_coefs_dict['H'], expected_nasa['H'])).all()
 
-
-def test_rxn_sys_irrev_reaction_antioch1():
+def test_rxn_sys_irrev_reaction_antioch():
     """Test against Antioch irrev rxn results"""
     xml_filename =  "tests/test_xml_files/rxns_irreversible_antioch.xml"
     xml_parser = XMLParser(xml_filename)
@@ -182,14 +195,6 @@ def test_rxn_sys_irrev_reaction_antioch1():
     assert numpy.isclose(rates['HO2'], 0.0000000000000000e+00, atol=1e-16)
     assert numpy.isclose(rates['H2O2'], 0.0000000000000000e+00, atol=1e-16)
 
-def test_rxn_sys_irrev_reaction_antioch2():
-    """Test against Antioch irrev rxn results"""
-    xml_filename =  "tests/test_xml_files/rxns_irreversible_antioch.xml"
-    xml_parser = XMLParser(xml_filename)
-    species = xml_parser.get_species()
-    sql_parser = SQLParser(TEST_DB_PATH, species)
-    thermo_coeffs = sql_parser.get_thermo_coeffs()
-
     # Condition #2
     temp = 2500.0000000000000000
     concentrations = ({'H': 5.0000000000000000e-01,
@@ -211,14 +216,6 @@ def test_rxn_sys_irrev_reaction_antioch2():
     assert numpy.isclose(rates['HO2'], -1.3498571473703539e+13, atol=1e-16)
     assert numpy.isclose(rates['H2O2'], -6.2652907852405969e+11, atol=1e-16)
 
-def test_rxn_sys_irrev_reaction_antioch3():
-    """Test against Antioch irrev rxn results"""
-    xml_filename =  "tests/test_xml_files/rxns_irreversible_antioch.xml"
-    xml_parser = XMLParser(xml_filename)
-    species = xml_parser.get_species()
-    sql_parser = SQLParser(TEST_DB_PATH, species)
-    thermo_coeffs = sql_parser.get_thermo_coeffs()
-
     # Condition #3 
     temp = 950.0000000000000000
     concentrations = ({'H': 5.0000000000000000e-01,
@@ -239,14 +236,6 @@ def test_rxn_sys_irrev_reaction_antioch3():
     assert numpy.isclose(rates['O2'], -1.3403448555187156e+13, atol=1e-16)
     assert numpy.isclose(rates['HO2'], 0.0000000000000000e+00, atol=1e-16)
     assert numpy.isclose(rates['H2O2'], 0.0000000000000000e+00, atol=1e-16)
-
-def test_rxn_sys_irrev_reaction_antioch4():
-    """Test against Antioch irrev rxn results"""
-    xml_filename =  "tests/test_xml_files/rxns_irreversible_antioch.xml"
-    xml_parser = XMLParser(xml_filename)
-    species = xml_parser.get_species()
-    sql_parser = SQLParser(TEST_DB_PATH, species)
-    thermo_coeffs = sql_parser.get_thermo_coeffs()
 
     # Condition #4
     temp = 950.0000000000000000
@@ -270,7 +259,7 @@ def test_rxn_sys_irrev_reaction_antioch4():
     assert numpy.isclose(rates['H2O2'], -3.3239141550534125e+11, atol=1e-16)
 
 
-def test_rxn_sys_rev_reaction_antioch1():
+def test_rxn_sys_rev_reaction_antioch():
     """Test against Antioch rev rxn results"""
     xml_filename =  "tests/test_xml_files/rxns_reversible_antioch.xml"
     xml_parser = XMLParser(xml_filename)
@@ -299,14 +288,6 @@ def test_rxn_sys_rev_reaction_antioch1():
     assert numpy.isclose(rates['HO2'], 1.5397482847664728e+09, atol=1e-16)
     assert numpy.isclose(rates['H2O2'], 0.0000000000000000e+00, atol=1e-16)
 
-def test_rxn_sys_rev_reaction_antioch2():
-    """Test against Antioch rev rxn results"""
-    xml_filename =  "tests/test_xml_files/rxns_reversible_antioch.xml"
-    xml_parser = XMLParser(xml_filename)
-    species = xml_parser.get_species()
-    sql_parser = SQLParser(TEST_DB_PATH, species)
-    thermo_coeffs = sql_parser.get_thermo_coeffs()
-
     # Condition #2
     temp = 2500.0000000000000000
     concentrations = ({'H': 5.0000000000000000e-01,
@@ -328,14 +309,6 @@ def test_rxn_sys_rev_reaction_antioch2():
     assert numpy.isclose(rates['HO2'], -1.3813504758333098e+13, atol=1e-16)
     assert numpy.isclose(rates['H2O2'], -3.0996344280845251e+11, atol=1e-16)
 
-def test_rxn_sys_rev_reaction_antioch3():
-    """Test against Antioch rev rxn results"""
-    xml_filename =  "tests/test_xml_files/rxns_reversible_antioch.xml"
-    xml_parser = XMLParser(xml_filename)
-    species = xml_parser.get_species()
-    sql_parser = SQLParser(TEST_DB_PATH, species)
-    thermo_coeffs = sql_parser.get_thermo_coeffs()
-
     # Condition #3
     temp = 950.0000000000000000
     concentrations = ({'H': 5.0000000000000000e-01,
@@ -356,14 +329,6 @@ def test_rxn_sys_rev_reaction_antioch3():
     assert numpy.isclose(rates['O2'], -1.3403448555203365e+13, atol=1e-16)
     assert numpy.isclose(rates['HO2'], 1.6208366095320475e+01, atol=1e-16)
     assert numpy.isclose(rates['H2O2'], 0.0000000000000000e+00, atol=1e-16)
-
-def test_rxn_sys_rev_reaction_antioch4():
-    """Test against Antioch rev rxn results"""
-    xml_filename =  "tests/test_xml_files/rxns_reversible_antioch.xml"
-    xml_parser = XMLParser(xml_filename)
-    species = xml_parser.get_species()
-    sql_parser = SQLParser(TEST_DB_PATH, species)
-    thermo_coeffs = sql_parser.get_thermo_coeffs()
 
     # Condition #4
     temp = 950.0000000000000000
