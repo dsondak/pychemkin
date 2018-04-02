d6085cac75a5dd3377a6f9ef8dc6b03b496045d4
FilipMichalsky <filip_michalsky@g.harvard.edu>
2018-04-01 23:37:36 -0400

    deleted files not pertinent to our work

--- pychemkin/config.py
+++
@@ -1,6 +0,0 @@
-
-import os
-
-# Thermodynamics database directory
-THIS_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
-DB_DIRECTORY = os.path.abspath(os.path.join(THIS_DIRECTORY, 'thermo_database'))
