e77956ed70d4f39d7842dbe6b4867a502d624078
Riddhi Shah <riddhishah@riddhis-mbp.lan>
2018-04-04 17:19:33 -0400

    Fix Missing Specie Bug & Scrapper

--- pychemkin/thermo_database/Scrapers.py
+++ pychemkin/thermo_database/Scrapers.py
@@ -69,9 +69,9 @@ class Scraper_7_coeffs:
                 raw_coeffs_body = child.get_text()
 
         #Separate the table
-        for idx,line in enumerate(raw_coeffs_body.split('\n')):
+        for idx,line in enumerate(self.web_page.text.split('\n')):
             if 'THE NUMBER PRECEDING EACH SPECIES IS THE CHEMICAL ABSTRACT' in line:
-                self.result = raw_coeffs_body.split('THE NUMBER PRECEDING EACH SPECIES IS THE CHEMICAL ABSTRACT (CAS) IDENTIFICATION.')[1]
+                self.result = self.web_page.text.split('THE NUMBER PRECEDING EACH SPECIES IS THE CHEMICAL ABSTRACT (CAS) IDENTIFICATION.')[1]
         f = open("7poly_scrapper_output.txt","w")
         f.write(self.result)
         f.close()
@@ -80,50 +80,50 @@ class Scraper_7_coeffs:
 class Scraper_9_coeffs:
     """
     Scraper for the 9-degree reaction coefficients from the BURCAT Database.
-    
+
     Requires following imports:
     from bs4 import BeautifulSoup
     import requests
-    
+
     The default url to scrape from is defaulted in initialization:
     http://garfield.chem.elte.hu/Burcat/NEWNASA.TXT
-    
+
     INPUTS
     =======
     The url where the database file resides.
-    
+
     RETURNS
     ========
     String with the database table, to be passed to the parser to create XML or SQL object
-    
+
     EXAMPLES
     =========
     Showing of the main scraper functionality. Returns true if everything runs properly.
-    
+
     >>> test_scraper = Scraper_9_coeffs(url='http://garfield.chem.elte.hu/Burcat/NEWNASA.TXT')
     >>> table_of_coeffs = test_scraper.scrape_coeffs()
     >>> table_of_coeffs.__class__.__name__=='str'
     True
     """
-    
+
     def __init__(self,url='http://garfield.chem.elte.hu/Burcat/NEWNASA.TXT'):
-        
+
         self.url = url
         self.result = None
         self.text_body = None
         self.webpage = None
         self.soup = None
-    
+
     def scrape_url(self):
         self.web_page = requests.get(self.url)
-        
+
     def create_soup_object(self):
         self.soup = BeautifulSoup(self.web_page.content, 'lxml')
-        
+
     def scrape_coeffs(self):
         self.scrape_url()
         self.create_soup_object()
-        
+
         my_tag = self.soup.find('egil.jahnsen')
 
         for item in my_tag.contents:
