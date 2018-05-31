ac6c20521afcd0b897171cfcb4580dadc24bd8a9
David Sondak <dsondak@seas.harvard.edu>
2018-05-31 09:56:54 -0400

    Missing files from scrapers branch.

---
+++ pychemkin/__init__.py
@@ -0,0 +1,9 @@
+
+
+
+from ._version import __version__
+from .config import *
+from .parsers import *
+from .reactions import *
+from .rxn_rate_coefficients import *
+from .thermo_database import *
---
+++ pychemkin/config.py
@@ -0,0 +1,10 @@
+import os
+
+# Thermodynamics database directory
+THIS_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+UNITS_DIRECTORY = os.path.abspath(os.path.join(THIS_DIRECTORY, 'units.csv'))
+DB_DIRECTORY = os.path.abspath(os.path.join(THIS_DIRECTORY, 'thermo_database'))
+
+# Constant(s)
+R_VALUE = 8.3144598 # Ideal gas constant, in J/mol/K (https://physics.nist.gov/cgi-bin/cuu/Value?r)
+EQ_TOL = 1e-16
