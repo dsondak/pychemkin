16b4f27d45660b041cc580afe5c262bfa222b87a
Riddhi Shah <riddhishah@riddhis-mbp.lan>
2018-05-30 21:08:03 -0400

    Delete all non-scraper files from scrapers branch

--- pychemkin/__init__.py
+++
@@ -1,9 +0,0 @@
-
-
-
-from ._version import __version__
-from .config import *
-from .parsers import *
-from .reactions import *
-from .rxn_rate_coefficients import *
-from .thermo_database import *
