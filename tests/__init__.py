1fbb2da636df5b33653b718e13f328d0f36c93a8
David Sondak <dsondak@seas.harvard.edu>
2018-05-31 10:48:08 -0400

    Removed reactions dependency.

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,7 +3,5 @@
 from pychemkin._version import __version__
 from pychemkin.config import *
 from pychemkin.parsers import *
-from pychemkin.reactions import *
-from pychemkin.rxn_rate_coefficients import *
 from pychemkin.thermo_database import *
 
